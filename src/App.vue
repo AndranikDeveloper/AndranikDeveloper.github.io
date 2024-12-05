a83de9a4968ac4103c4c7459a6d9724189342fc0
AndranikDeveloper <andranikagabekyan2005@gmail.com>
2024-12-05 19:10:32 +0400

    Test task

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+// import { RouterLink } from 'vue-router'
+import NotesList from './views/NotesList.vue'
+</script>
+
+<template>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 10px;
+}
+</style>
